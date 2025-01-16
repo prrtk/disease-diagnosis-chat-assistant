e197ea757aa93ac00f6cfbfe6daa4f93093b0a5c
Pratik Choudhury <85783701+prrtk@users.noreply.github.com>
2025-01-16 22:18:05 +0530

    Initial project setup

---
+++ verify_setup.py
@@ -0,0 +1,11 @@
+# Import all the libraries to verify their installation
+import pandas as pd
+import numpy as np
+import sklearn
+import flask
+import openai
+import matplotlib
+import seaborn
+
+# Print success message
+print("All dependencies are installed correctly!")
